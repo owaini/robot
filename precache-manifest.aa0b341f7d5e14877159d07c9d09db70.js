f2f0a473fcdd1b3464a1af0f97e017267d00eaf0
owaini <omar.teachcampus@gmail.com>
2020-09-04 20:21:43 +0300

    Updates

---
+++ precache-manifest.aa0b341f7d5e14877159d07c9d09db70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9dff65114adb0ba8f47ee9bd75b8944",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "01be661733d45e485a8e",
+    "url": "/robot/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "67e44995db16f0e21368",
+    "url": "/robot/static/css/main.d9ed4be5.chunk.css"
+  },
+  {
+    "revision": "01be661733d45e485a8e",
+    "url": "/robot/static/js/2.14bc996e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot/static/js/2.14bc996e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e44995db16f0e21368",
+    "url": "/robot/static/js/main.ef04012e.chunk.js"
+  },
+  {
+    "revision": "85462878c18e2297f885",
+    "url": "/robot/static/js/runtime-main.9354cafe.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
